9fe22e823a8391aea617191306ca44000fdb8c42
Rob Marable <rmarable@iseatz.com>
2011-09-30 14:44:19 -0500

    Simplified camelize

--- lib/protocol_buffers/compiler/file_descriptor_to_ruby.rb
+++ lib/protocol_buffers/compiler/file_descriptor_to_ruby.rb
@@ -168,12 +168,8 @@ HEADER
     "#{s[0,1].capitalize}#{s[1..-1]}" if s
   end
   
-  def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
-    if first_letter_in_uppercase
-      lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-    else
-      lower_case_and_underscored_word.to_s[0].chr.downcase + camelize(lower_case_and_underscored_word)[1..-1]
-    end
+  def camelize(lower_case_and_underscored_word)
+    lower_case_and_underscored_word.to_s.gsub(/(?:^|_)(.)/) { $1.upcase }
   end
 
 end
